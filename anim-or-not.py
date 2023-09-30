a486726a2b0240a71df9a6be1811d0c1d1b19f24
NASTY-SMIT <nasty.smit.2004@mail.ru>
2023-09-30 16:05:47 +0700

    ww

---
+++ anim-or-not.py
@@ -0,0 +1,78 @@
+import os
+import torch
+import torch.nn as nn 
+import torch.optim as optim
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+from tqdm import tqdm
+
+# %matplotlib inline
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def image_shower(images, labels, n=8):
+    plt.figure(figsize=(12, 12))
+    for i, image in enumerate(images[:n]):
+        plt.subplot(n, n, i + 1)
+        image = image / 2 + 0.5
+        plt.imshow(image.numpy().transpose((1, 2, 0)).squeeze())
+    print('Real labels: ', ' '.join('%5s' % classes[label] for label in labels[:n]))
+
+classes = ("With animal", "Zero animal")
+PATH = './animals'
+
+transform = transforms.Compose(
+    [transforms.Resize((64,64)),
+     transforms.ToTensor(),
+     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+
+trainset = torchvision.datasets.ImageFolder(os.path.join(PATH, 'train'), transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, num_workers=0, shuffle=True)
+
+testset = torchvision.datasets.ImageFolder(os.path.join(PATH, 'test'), transform=transform)
+testloader = torch.utils.data.DataLoader(trainset, batch_size=64, num_workers=0, shuffle=True)
+
+images, labels = next(iter(trainloader))
+image_shower(images, labels)
+
+
+model = torchvision.models.resnet18(pretrained=True)
+for param in model.parameters():
+    param.requires_grad = False
+model.fc = nn.Linear(512, 2)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+epochs = 10
+model.to(device)
+
+for epoch in range(epochs):
+    running_loss = 0.0
+    for i, data in tqdm(enumerate(trainloader)):
+        inputs, labels = data[0].to(device), data[1].to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print('Epoch {} - Training loss: {} '.format(epoch, running_loss/len(trainloader)))
+
+correct = 0
+total = 0
+with torch.no_grad():
+    model.eval()
+    for data in testloader:
+        inputs, labels = data[0].to(device), data[1].to(device)
+        outputs = model(inputs)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+print('Accuracy: %d' % (100 * correct/total))
--- model-obuch.py
+++ model-obuch.py
@@ -9,26 +9,19 @@ train_folder = './train'
 val_folder = './val'
 images_folder = "./images"
 
-# Получение списка файлов с расширением .jpg из папки train
 train_files = [f for f in os.listdir(train_folder) if f.endswith('.jpg')]
 
-# Получение полных путей к файлам
 train_file_paths = [os.path.join(train_folder, f) for f in train_files]
 
-# Создание списка меток (0 - качественные фотографии, 1 - некачественные фотографии)
 train_labels = [0] * len(train_file_paths)
 
 
-# Получение списка файлов с расширением .jpg из папки val
 val_files = [f for f in os.listdir(val_folder) if f.endswith('.jpg')]
 
-# Получение полных путей к файлам
 val_file_paths = [os.path.join(val_folder, f) for f in val_files]
 
-# Создание списка меток (0 - качественные фотографии, 1 - некачественные фотографии)
 val_labels = [1] * len(val_file_paths)
 
-# Преобразование фотографии в тензор и нормализация
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -46,9 +39,8 @@ val_data = []
 for file_path in val_file_paths:
     img = Image.open(file_path)
     img_tensor = transform(img)
-    val_data.append((img_tensor, 1))
+    val_data.append((img_tensor, 0))
 
-# Создание итераторов по данным
 train_dataloader = torch.utils.data.DataLoader(train_data,
                                                batch_size=32, shuffle=True)
 val_dataloader = torch.utils.data.DataLoader(val_data,
@@ -66,8 +58,7 @@ model.fc = torch.nn.Linear(num_features, 2)
 criterion = torch.nn.CrossEntropyLoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-# Обучение модели
-num_epochs = 10
+num_epochs = 20
 for epoch in range(num_epochs):
     model.train()
     for images, labels in train_dataloader:
@@ -85,25 +76,19 @@ for epoch in range(num_epochs):
         correct = 0
         total = 0
         for images, labels in val_dataloader:
-            # Предсказание класса изображения с помощью модели
             outputs = model(images)
             _, predicted = torch.max(outputs, 1)
 
-            # Вычисление точности модели
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
         accuracy = correct / total
         print(f"Epoch {epoch+1}/{num_epochs}, Validation Accuracy: {accuracy}")
 
-# Классификация фотографий из папки images
-# Получение списка файлов с расширением .jpg из папки images
 images_files = [f for f in os.listdir(images_folder) if f.endswith('.jpg')]
 
-# Получение полных путей к файлам
 images_file_paths = [os.path.join(images_folder, f) for f in images_files]
 
-# Преобразование фотографии в тензор и нормализация
 transform = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
@@ -120,18 +105,11 @@ for file_path in images_file_paths:
 images_dataloader = torch.utils.data.DataLoader(images_data,
                                                 batch_size=1, shuffle=False)
 
-# test_dataset = torchvision.datasets.ImageFolder(images_folder,
-                                                # transform=transform)
-# test_dataloader = torch.utils.data.DataLoader(test_dataset,
-                                              # batch_size=1, shuffle=False)
-
 model.eval()
 for images, _ in images_dataloader:
-    # Предсказание класса изображения с помощью модели
     outputs = model(images)
     _, predicted = torch.max(outputs, 1)
 
-    # Вывод результата классификации
     if predicted == 0:
         print("Качественная фотография")
     else:
