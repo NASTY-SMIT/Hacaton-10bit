947f46e000f177930b0400d15381a32daac8e450
8bRfg <132456568+8bRfg@users.noreply.github.com>
2023-10-01 11:02:29 +0700

    Actual version

---
+++ anim-or-not-v2-p1(Train).py
@@ -0,0 +1,49 @@
+import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.transforms as transforms
+from tqdm import tqdm
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+PATH = './animals'
+
+transform = transforms.Compose(
+    [transforms.Resize((128, 128)),
+     transforms.ToTensor(),
+     transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+
+trainset = torchvision.datasets.ImageFolder(os.path.join(PATH, 'train'),
+                                            transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=128,
+                                          num_workers=0, shuffle=False)
+
+model = torchvision.models.resnet18(pretrained=True)
+for param in model.parameters():
+    param.requires_grad = True
+model.fc = nn.Linear(512, 2)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+epochs = 10
+model.to(device)
+
+for epoch in range(epochs):
+    running_loss = 0.0
+    for i, data in tqdm(enumerate(trainloader)):
+        inputs, labels = data[0].to(device), data[1].to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    print('Epoch {} - Training loss: {}'.format(epoch, running_loss/len(trainloader)))
+
+# Сохранение модели
+torch.save(model.state_dict(), 'model.pth')
